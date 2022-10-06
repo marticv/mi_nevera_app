04415bf50b97cf7572a4dc71aff9822b5d756316
Marti Curto <marti_cv@hotmail.com>
2022-10-06 22:10:26 +0200

    modificamos recetas e ingredientes para que id pueda ser null

--- app/src/main/java/com/proyecto_linkia/mi_nevera_app/clases/Ingrediente.kt
+++ app/src/main/java/com/proyecto_linkia/mi_nevera_app/clases/Ingrediente.kt
@@ -1,5 +1,5 @@
 package com.proyecto_linkia.mi_nevera_app.clases
 
-class Ingrediente(var id_ingrediente: Int, var nombre: String) {
+class Ingrediente(var id_ingrediente: Int?, var nombre: String) {
 
 }

07718c9bb3588000c7a4b90bf791cd2f8ae7e414
Marti Curto <marti_cv@hotmail.com>
2023-03-03 15:52:57 +0100

    adding entities to room

---
+++ app/src/main/java/com/proyecto_linkia/mi_nevera_app/data/db/dao/IngredientDao.kt
@@ -0,0 +1,16 @@
+package com.proyecto_linkia.mi_nevera_app.data.db.dao
+
+import androidx.room.*
+import com.proyecto_linkia.mi_nevera_app.data.db.entities.IngredientEntity
+
+@Dao
+interface IngredientDao {
+    @Query("SELECT * FROM ingredients_table")
+    suspend fun getAllMyIngredients():List<IngredientEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertEmptyRecipe(ingredientEntity: IngredientEntity)
+
+    @Delete
+    suspend fun deleteEmptyRecipe(ingredientEntity: IngredientEntity)
+}
--- app/src/main/java/com/proyecto_linkia/mi_nevera_app/data/db/database/AppDB.kt
+++ app/src/main/java/com/proyecto_linkia/mi_nevera_app/data/db/database/AppDB.kt
@@ -2,13 +2,19 @@ package com.proyecto_linkia.mi_nevera_app.data.db.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.proyecto_linkia.mi_nevera_app.data.db.dao.EmptyRecipeDao
+import com.proyecto_linkia.mi_nevera_app.data.db.dao.IngredientDao
 import com.proyecto_linkia.mi_nevera_app.data.db.dao.MyIngredientDao
+import com.proyecto_linkia.mi_nevera_app.data.db.entities.EmptyRecipeEntity
+import com.proyecto_linkia.mi_nevera_app.data.db.entities.IngredientEntity
 import com.proyecto_linkia.mi_nevera_app.data.db.entities.MyIngredient
 
 
 @Database(
-    entities = [MyIngredient::class],
+    entities = [MyIngredient::class,IngredientEntity::class,EmptyRecipeEntity::class],
     version = 1)
 abstract class AppDB:RoomDatabase() {
     abstract fun getMyIngredientDao():MyIngredientDao
+    abstract fun getEmptyRecipeDao():EmptyRecipeDao
+    abstract fun getIngredientsDao():IngredientDao
 }

04bba51806abee35cc76bfae83755acc8cf612c8
Marti Curto <marti_cv@hotmail.com>
2023-03-31 17:16:48 +0200

    added fragment menu

--- app/src/main/java/com/proyecto_linkia/mi_nevera_app/RecipeInformation.kt
+++ app/src/main/java/com/proyecto_linkia/mi_nevera_app/RecipeInformation.kt
@@ -5,12 +5,9 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.DisplayMetrics
 import androidx.appcompat.app.AppCompatActivity
-import com.google.gson.internal.bind.util.ISO8601Utils.format
 import com.proyecto_linkia.mi_nevera_app.clases.Recipe
 import com.proyecto_linkia.mi_nevera_app.databinding.ActivityRecipeInformationBinding
 import com.squareup.picasso.Picasso
-import java.text.DateFormat
-import java.util.*
 
 
 class RecipeInformation : AppCompatActivity() {
@@ -39,7 +36,7 @@ class RecipeInformation : AppCompatActivity() {
 
         var ingredients =""
         for (ingredient in recipe.ingredients){
-            ingredients+=ingredient
+            ingredients+=ingredient+"\n"
         }
         binding.tvIngredients.text = ingredients
 
